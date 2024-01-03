08b11b4f556e7efdce3e71c4cc32bc124951e65b
Rohit Kumar <123821699+rohitt24k@users.noreply.github.com>
2024-01-03 17:07:28 +0530

    feat: added the homepage (#4)

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/login/login";
+import Homepage from "./pages/homepage/homepage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
-        {/* <Route path="/" element=<Homepage /> /> */}
+        <Route path="/" element=<Homepage /> />
         {/* <Route path="/message" element=<Message /> /> */}
       </Routes>
     </BrowserRouter>
