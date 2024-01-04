8e84767b5054b47dfaaa39f687b642d8ecdb65ce
Afeefuddin <126336384+afeefuddin@users.noreply.github.com>
2024-01-04 15:16:55 +0530

    Afeeftwo (#9)
    * Initialised The Email Server
    
    * Completed the Email Server
    
    * Added the websocket server

---
+++ Email-Server/utils/welcomeEmail.js
@@ -0,0 +1,24 @@
+const cheerio = require('cheerio');
+
+function welcomeEmail(score, analysis,articles){
+const email =  `
+<html>
+    Hi,
+    Thank you for Joining <b>Mind Mate </b>.
+    We hope you are doing better than before.
+    Here is your Mental health report: 
+        Score  : ${score} <br/>
+        Analysis : ${analysis}
+    Here Are Some Article we suggest you to go through,these are compiled Articles for you which can help you to improve your Mental health
+    ${articles}
+</html>
+`
+const $ = cheerio.load(email);
+
+    const updatedEmailHTML = $.html();
+    
+
+    return updatedEmailHTML ;
+}
+
+module.exports = {welcomeEmail}
