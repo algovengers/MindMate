fdaae30d6fce8d3b7d4f500d3d0d6ebbfb5da545
Afeefuddin <afeefud2004din@gmail.com>
2024-01-04 21:15:25 +0530

    Email Server

--- Email-Server/index.js
+++ Email-Server/index.js
@@ -22,6 +22,9 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
+app.get('/',(req,res)=>{
+    res.status(200).json({"message" : "Hello"})
+})
 
 app.post('/welcomeEmail',sendWelcomeEmail)
 connectDB()
