6ceff0fea3e86ed2cf4a0c7ef3d13519cf6bfdf4
subharthi75 <subharthi75@gmail.com>
2024-01-04 18:31:14 +0530

    frontend refactor and bug fix and repositioning

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import {CookiesProvider} from 'react-cookie'
 import ContextProvider from "./context/contextProvider";
 
 
--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-    ],
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+    },
   },
   plugins: [],
 }
