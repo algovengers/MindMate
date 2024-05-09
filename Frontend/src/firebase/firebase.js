d77c57168e9929306396455753658ab79ed8bc3a
Subharthi Hazra <92659226+subharthihazra@users.noreply.github.com>
2024-05-09 12:20:31 +0530

    minor fix

--- Frontend/src/firebase/firebase.js
+++ Frontend/src/firebase/firebase.js
@@ -17,13 +17,13 @@ import axios from "axios";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB_STKtkXoNYXUMDIAqQ-e6kOGS8fI2wcc",
-  authDomain: "mindmate-algovengers.firebaseapp.com",
-  projectId: "mindmate-algovengers",
-  storageBucket: "mindmate-algovengers.appspot.com",
-  messagingSenderId: "194388833270",
-  appId: "1:194388833270:web:5ac806ba946f8339d44dd7",
-  measurementId: "G-CZ8NCME35H",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
