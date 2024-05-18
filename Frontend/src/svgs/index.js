4cc70ed8353cee5fc8dc4b41ccbe7fe149a218f5
Sujal Raunak <sujalraunak452@gmail.com>
2024-05-18 18:36:05 +0530

    Designed footer solved issue

---
+++ Frontend/src/svgs/index.js
@@ -0,0 +1,14 @@
+import facebook from "./facebook.svg";
+import instagram from "./instagram.svg";
+import linkedin from "./linkedin.svg";
+import twitter from "./twitter.svg";
+import logo from "./logo.png";
+
+export {
+    
+    facebook,
+    instagram,
+    linkedin,
+    twitter,
+    logo,
+  };
